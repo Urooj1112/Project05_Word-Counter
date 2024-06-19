ae13da46f02fc0181ecb48be54d18fda6068e957
Urooj Saeed <165631956+Urooj1112@users.noreply.github.com>
2024-06-19 01:25:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.greenBright("\n \t\tProject-06 Word Counter"));
+console.log("*************************************");
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter Words Or Sentence: ",
+    }
+]);
+let words = answers.sentence.trim().split(" ");
+console.log("*************************************");
+console.log(chalk.bold("Sentence words: "));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.greenBright(words.length)}`));
+console.log("*************************************");
